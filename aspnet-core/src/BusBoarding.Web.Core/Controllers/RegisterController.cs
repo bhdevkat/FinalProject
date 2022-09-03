68ae63b9a49dffb43730bc441d819b25b8cf1b15
KATLEGO RAMBAU <43708141+bhdevkat@users.noreply.github.com>
2022-09-03 10:58:46 +0200

    last

--- aspnet-core/src/BusBoarding.Web.Core/Controllers/RegisterController.cs
+++ aspnet-core/src/BusBoarding.Web.Core/Controllers/RegisterController.cs
@@ -1,7 +1,9 @@
 ﻿using Abp.Domain.Repositories;
 using Abp.Web.Models;
 using BusBoardingSystem.Domain;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Threading.Tasks;
 
 namespace BusBoarding.Controllers
@@ -21,6 +23,23 @@ namespace BusBoarding.Controllers
         {
             var student = await _studentTrpository.GetAsync(studentId);
             return student;
+        }
+
+        [HttpPost(template: "/api/client/v1/test")]
+        public async Task<ActionResult<int>> CreateEmployee(int emp)
+        {
+            try
+            {
+                if (emp == null)
+                    return BadRequest();
+
+                return Ok(emp * 2);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error creating new employee record");
+            }
         }
     }
 }
